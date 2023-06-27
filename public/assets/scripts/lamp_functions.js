737007c90c5af0e565c63d61680c1a0d7c22dc75
fso1007 <fabio_oliveira10@live.com>
2023-06-27 14:25:36 -0300

    first commit

---
+++ public/assets/scripts/lamp_functions.js
@@ -0,0 +1,56 @@
+// HTML img element
+const lamp = document.querySelector('.lamp__img');
+
+// IMG Sources
+const lampOnImg = '/public/assets/images/light-on.svg';
+const lampOffImg = '/public/assets/images/light-off.svg';
+const lampBrokenImg = '/public/assets/images/light-broken.svg';
+
+// Buttons
+const buttonOn = document.querySelector('.button__on');
+const buttonOff = document.querySelector('.button__off');
+const buttonOnOff = document.querySelector('.button__on-off')
+
+// Event Listeners
+buttonOn.addEventListener('click', turnLampOn);
+buttonOff.addEventListener('click', turnLampOff);
+buttonOnOff.addEventListener('click', turnLampOnOff)
+lamp.addEventListener('mouseover', turnLampOn);
+lamp.addEventListener('mouseleave', turnLampOff);
+lamp.addEventListener('dblclick', brokeLamp);
+
+function turnLampOn() {
+  if (!isLampBroken()) {
+    lamp.src = lampOnImg;
+  }
+}
+
+function turnLampOff() {
+  if (!isLampBroken()) {
+    lamp.src = lampOffImg;
+  }
+}
+
+function brokeLamp() {
+  lamp.src = lampBrokenImg;
+}
+
+function isLampBroken() {
+  return lamp.src.includes('broken');
+}
+
+function isLampOn() {
+  return lamp.src.includes('on');
+}
+
+
+function turnLampOnOff() {
+ if (!isLampOn()) {
+    turnLampOn();
+
+  } else {
+    turnLampOff();
+  }
+}
+
+// Dificuldades: A parte mais difícil foi manter a lÂmpada quebrada após o double click, pois os eventlisteners pegavam o mouseover e mouseleave e a imagem era trocada
